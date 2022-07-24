17379f3574ea718f8a2876106987ac7b50c3a1a6
Jan Malej <jan.malej12@gmail.com>
2022-07-24 13:53:59 +0200

    V modelu dodal razred predmet

--- model.py
+++ model.py
@@ -1,2 +1,10 @@
 class Predmet:
-    pass
+    def __init__(self, ime, slovar={}):
+        self.ime = ime
+        self.ocene = slovar
+
+    def dodaj(self, opis, kolicina):
+        if opis in self.ocene.keys():
+            return "Ta ocena je že vnešena."
+        else:
+            self.ocene[opis] = kolicina
