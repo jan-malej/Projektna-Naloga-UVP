694ecd8678dfe87ef05066df5d8d6934c615f612
Jan Malej <jan.malej12@gmail.com>
2022-08-19 15:04:33 +0200

    spisek potrebnih funkcij

--- tekstovni_vmesnik.py
+++ tekstovni_vmesnik.py
@@ -1,4 +1,40 @@
-from model import Stanje
+import model
 
 DATOTEKA_S_STANJEM = "stanje.json"
-stanje = Stanje.preberi_iz_datoteke(DATOTEKA_S_STANJEM)
+stanje = model.Stanje.preberi_iz_datoteke(DATOTEKA_S_STANJEM)
+
+def dodaj_solsko_leto():
+    pass
+
+def odstrani_solsko_leto():
+    pass
+
+def dodaj_predmet():
+    pass
+
+def odstrani_predmet():
+    pass
+
+def dodaj_rezultat():
+    pass
+
+def odstrani_rezultat():
+    pass
+
+def vpisi_oceno():
+    pass
+
+def izbrisi_oceno():
+    pass
+
+def nastavi_stevilo_kolokvijev():
+    pass
+
+def izpis_povprecja_ocen():
+    pass
+
+def izpis_povprecja_rezultatov():
+    pass
+
+def koliko_potrebujem():
+    pass
