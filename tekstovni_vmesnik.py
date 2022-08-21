5a97d301630f2bdc831d7dd8f7ef7bf375397d6b
Jan Malej <jan.malej12@gmail.com>
2022-08-21 13:16:54 +0200

    funkcije v tekstovnem vmesniku

--- tekstovni_vmesnik.py
+++ tekstovni_vmesnik.py
@@ -15,7 +15,10 @@ def izbrisi_solsko_leto():
     stanje.izbrisi_solsko_leto(ime)
 
 def dodaj_predmet():
-    pass
+    ime = input('Ime predmeta>')
+    st_kolokvijev = input('Število kolokvijev')
+    st_ocen = input('Število ocen')
+    stanje.dodaj_predmet(ime, st_kolokvijev, st_ocen)
 
 def odstrani_predmet():
     pass
@@ -23,23 +26,33 @@ def odstrani_predmet():
 def dodaj_rezultat():
     ime_predmeta = input('Kateremu predmetu želite dodati rezultat?')
     opis = input('Opis rezultata>')
-    kolicina = input('Vnesite rezultat')
+    kolicina = input('Vpišite rezultat')
     stanje.dodaj_rezultat(ime_predmeta, opis, kolicina)
 
 def odstrani_rezultat():
-    pass
+    ime_predmeta = input('Kateremu predmetu želite odstraniti rezultat?')
+    opis = input('Vpišite opis rezultata, ki ga želite izbrisati.')
+    stanje.odstrani_rezultat(ime_predmeta, opis)
 
 def vpisi_oceno():
-    pass
+    ime_predmeta = input('Kateremu predmetu želite vpisati oceno?')
+    opis = input('Vpišite opis ocene.')
+    kolicina = input('Vpišite oceno.')
+    stanje.vpisi_oceno(ime_predmeta, opis, kolicina)
 
 def izbrisi_oceno():
-    pass
+    ime_predmeta = input('Kateremu predmetu želite izbrisati oceno?')
+    opis = input('Vpišite opis ocene, ki jo želite izbrisati.')
+    stanje.izbrisi_oceno(ime_predmeta, opis)
 
 def nastavi_stevilo_kolokvijev():
-    pass
+    ime_predmeta = input('Kateremu predmetu želite nastaviti število kolokvijev?')
+    st = int(input('Vpišite število kolokvijev.'))
+    stanje.nastavi_st_kolokvijev(ime_predmeta, st)
 
 def izpis_povprecja_ocen():
-    pass
+    povprecje_ocen = stanje.aktualno_solsko_leto.povprecje()
+    print(f"Trenutno povprečje ocen: {povprecje_ocen}")
 
 def izpis_povprecja_rezultatov():
     pass
