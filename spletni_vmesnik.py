5c90e0efc20f78cc4604c9e964d613df83baa291
Jan Malej <jan.malej12@gmail.com>
2022-08-21 18:59:35 +0200

    napisal base.html v ostalih html datotekah rebase

--- spletni_vmesnik.py
+++ spletni_vmesnik.py
@@ -1,10 +1,10 @@
 import bottle
 import model
 IME_DATOTEKE_S_STANJEM = 'stanje.json'
-stanje = model.Stanje.preberi_iz_datoteke(IME_DATOTEKE_S_STANJEM)
+#stanje = model.Stanje.preberi_iz_datoteke(IME_DATOTEKE_S_STANJEM)
 
 @bottle.get('/')
 def osnovna_stran():
     return bottle.template('osnova.html')
-    
+
 bottle.run(debug=True, reloader=True)
