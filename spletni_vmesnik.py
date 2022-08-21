0266bbab16d867edf4997ca771f6c0b75df31520
Jan Malej <jan.malej12@gmail.com>
2022-08-21 18:48:55 +0200

    osnovna stran v spletni vmesnik

--- spletni_vmesnik.py
+++ spletni_vmesnik.py
@@ -3,5 +3,8 @@ import model
 IME_DATOTEKE_S_STANJEM = 'stanje.json'
 stanje = model.Stanje.preberi_iz_datoteke(IME_DATOTEKE_S_STANJEM)
 
-
+@bottle.get('/')
+def osnovna_stran():
+    return bottle.template('osnova.html')
+    
 bottle.run(debug=True, reloader=True)
