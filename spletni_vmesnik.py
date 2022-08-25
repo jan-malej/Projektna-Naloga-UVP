596dda530f7de74ab3bff6af7f6c85dc512489fc
Jan Malej <jan.malej12@gmail.com>
2022-08-25 20:02:45 +0200

    dodal piskotke in prijavo za vec  uporabnikov

--- spletni_vmesnik.py
+++ spletni_vmesnik.py
@@ -1,7 +1,8 @@
 import bottle
 import model
 IME_DATOTEKE_S_STANJEM = 'stanje.json'
-stanje = model.Stanje.preberi_iz_datoteke(IME_DATOTEKE_S_STANJEM)
+SIFRA = 'Posebna skrivna koda'
+GESLO = 'geslo'
 
 def preveri_int(vnos):
     """Preveri, če se vnos pretvori v int, ki je večji od 0"""
@@ -33,38 +34,78 @@ def je_neprazen_niz(niz):
         return False
     return True
 
-def shrani():
-    stanje.zapisi_v_datoteko(IME_DATOTEKE_S_STANJEM)
+def ime_uporabnikove_datoteke(username):
+    return f"Uporabniki/{username}.json"
+
+def trenutni_uporabnik_stanje():
+    username = bottle.request.get_cookie('uporabnisko_ime', secret=SIFRA)
+    if username == None:
+        bottle.redirect('/login/')
+    ime_datoteke = ime_uporabnikove_datoteke(username)
+    try:
+        stanje = model.Stanje.preberi_iz_datoteke(ime_datoteke)
+    except FileNotFoundError:
+        stanje = model.Stanje()
+        stanje.zapisi_v_datoteko(ime_datoteke)
+    return stanje
+
+def shrani(stanje):
+    username = bottle.request.get_cookie('uporabnisko_ime', secret=SIFRA)
+    ime_datoteke = ime_uporabnikove_datoteke(username)
+    stanje.zapisi_v_datoteko(ime_datoteke)
 
 @bottle.get("/Static/<file>")
 def staticni_css(file):
     return bottle.static_file(file, root="Static")
 
+@bottle.get('/login/')
+def login():
+    return bottle.template('log-in.html')
+
+@bottle.post('/login/')
+def login_post():
+    username = bottle.request.forms.getunicode('username')
+    geslo = bottle.request.forms.getunicode('geslo')
+    if je_neprazen_niz(username) and geslo == GESLO:
+        bottle.response.set_cookie('uporabnisko_ime', username, path='/', secret=SIFRA)
+        bottle.redirect('/')
+    else:
+        return "Napaka ob prijavi"
+
+@bottle.post('/logout/')
+def logout_post():
+    bottle.response.delete_cookie('uporabnisko_ime', path='/')
+    bottle.redirect('/')
+
 @bottle.get('/')
 def osnovna_stran():
+    stanje = trenutni_uporabnik_stanje()
     return bottle.template('osnova.html', leta = stanje.solska_leta)
 
 @bottle.post('/dodaj_solsko_leto/')
 def dodaj_solsko_leto():
+    stanje = trenutni_uporabnik_stanje()
     ime = bottle.request.forms.getunicode('Ime')
     if je_neprazen_niz(ime):
         stanje.dodaj_solsko_leto(ime)
-        shrani()
+        shrani(stanje)
         bottle.redirect('/')
     else:
         bottle.redirect('/')
 
 @bottle.post('/izbrisi_solsko_leto/')
 def izbrisi_solsko_leto():
+    stanje = trenutni_uporabnik_stanje()
     if not je_neprazen_seznam(stanje.solska_leta):
         bottle.redirect('/')
     ime = bottle.request.forms.getunicode('Ime')
     stanje.izbrisi_solsko_leto(ime)
-    shrani()
+    shrani(stanje)
     bottle.redirect('/')
 
 @bottle.get('/solsko_leto/<index:int>/')
 def solsko_leto(index):
+    stanje = trenutni_uporabnik_stanje()
     aktualno = stanje.solska_leta[index]
     stanje.nastavi_aktualno(aktualno)
     return bottle.template(
@@ -75,6 +116,7 @@ def solsko_leto(index):
 
 @bottle.post('/solsko_leto/<index:int>/dodaj_predmet/')
 def dodaj_predmet(index):
+    stanje = trenutni_uporabnik_stanje()
     aktualno = stanje.solska_leta[index]
     stanje.nastavi_aktualno(aktualno)
     ime = bottle.request.forms.getunicode('ime')
@@ -82,22 +124,24 @@ def dodaj_predmet(index):
     if not preveri_int(st):
         bottle.redirect(f'/solsko_leto/{index}/')
     stanje.dodaj_predmet(ime, int(st))
-    shrani()
+    shrani(stanje)
     bottle.redirect(f'/solsko_leto/{index}/')
 
 @bottle.post('/solsko_leto/<index:int>/odstrani_predmet/')
 def izbrisi_predmet(index):
+    stanje = trenutni_uporabnik_stanje()
     aktualno = stanje.solska_leta[index]
     stanje.nastavi_aktualno(aktualno)
     if not je_neprazen_seznam(aktualno.predmeti):
         bottle.redirect(f'/solsko_leto/{index}/')
     ime = bottle.request.forms.getunicode('ime')
     stanje.odstrani_predmet(ime)
-    shrani()
+    shrani(stanje)
     bottle.redirect(f'/solsko_leto/{index}/')    
 
 @bottle.post('/solsko_leto/<index:int>/vpisi_oceno/')
 def vpisi_oceno(index):
+    stanje = trenutni_uporabnik_stanje()
     aktualno = stanje.solska_leta[index]
     stanje.nastavi_aktualno(aktualno)
     if not je_neprazen_seznam(aktualno.predmeti):
@@ -107,13 +151,14 @@ def vpisi_oceno(index):
     kol = bottle.request.forms.getunicode('kol')
     if je_neprazen_niz(opis) and preveri_int(kol):
         stanje.vpisi_oceno(ime_pr, opis, int(kol))
-        shrani()
+        shrani(stanje)
         bottle.redirect(f'/solsko_leto/{index}/')
     else:
         bottle.redirect(f'/solsko_leto/{index}/')
 
 @bottle.post('/solsko_leto/<index:int>/dodaj_rezultat/')
 def dodaj_rezultat(index):
+    stanje = trenutni_uporabnik_stanje()
     aktualno = stanje.solska_leta[index]
     stanje.nastavi_aktualno(aktualno)
     if not je_neprazen_seznam(aktualno.predmeti):
@@ -123,13 +168,14 @@ def dodaj_rezultat(index):
     kol = bottle.request.forms.getunicode('kol')
     if je_neprazen_niz(opis) and preveri_float(kol):
         stanje.dodaj_rezultat(ime_pr, opis, float(kol))
-        shrani()
+        shrani(stanje)
         bottle.redirect(f'/solsko_leto/{index}/')
     else:
         bottle.redirect(f'/solsko_leto/{index}/')
 
 @bottle.post('/solsko_leto/<index:int>/izbrisi_oceno/')
 def izbrisi_oceno(index):
+    stanje = trenutni_uporabnik_stanje()
     aktualno = stanje.solska_leta[index]
     stanje.nastavi_aktualno(aktualno)
     if not je_neprazen_seznam(aktualno.predmeti):
@@ -139,11 +185,12 @@ def izbrisi_oceno(index):
     if not je_neprazen_niz(opis):
         bottle.redirect(f'/solsko_leto/{index}/')
     stanje.izbrisi_oceno(ime_pr, opis)
-    shrani()
+    shrani(stanje)
     bottle.redirect(f'/solsko_leto/{index}/')
 
 @bottle.post('/solsko_leto/<index:int>/odstrani_rezultat/')
 def odstrani_rezultat(index):
+    stanje = trenutni_uporabnik_stanje()
     aktualno = stanje.solska_leta[index]
     stanje.nastavi_aktualno(aktualno)
     if not je_neprazen_seznam(aktualno.predmeti):
@@ -153,11 +200,12 @@ def odstrani_rezultat(index):
     if not je_neprazen_niz(opis):
         bottle.redirect(f'/solsko_leto/{index}/')
     stanje.odstrani_rezultat(ime_pr, opis)
-    shrani()
+    shrani(stanje)
     bottle.redirect(f'/solsko_leto/{index}/')
 
 @bottle.post('/solsko_leto/<index:int>/nastavi_kolokvije/')
 def nastavi_st_kolokvijev(index):
+    stanje = trenutni_uporabnik_stanje()
     aktualno = stanje.solska_leta[index]
     stanje.nastavi_aktualno(aktualno)
     if not je_neprazen_seznam(aktualno.predmeti):
@@ -167,7 +215,7 @@ def nastavi_st_kolokvijev(index):
     if not preveri_int(st):
         bottle.redirect(f'/solsko_leto/{index}/')
     stanje.nastavi_st_kolokvijev(ime_pr, int(st))
-    shrani()
+    shrani(stanje)
     bottle.redirect(f'/solsko_leto/{index}/')
 
 bottle.run(debug=True, reloader=True)
